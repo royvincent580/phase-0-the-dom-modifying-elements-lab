56b5721dffea90a37267d68c0357ab2f124d57d5
royvincent580 <vincentroy580@gmail.com>
2025-06-27 22:53:09 +0300

    dom

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+// Remove the <main id="main"> element from the DOM
+const main = document.getElementById('main');
+if (main) {
+  main.remove();
+}
+
+// Create a new <h1> element and assign it to 'newHeader'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Roy-Vincent is the champion';
